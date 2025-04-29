d1d7a872e538cedf3c0a93f2d64ffda73e13d7db
stb369 <35536182+stb369@users.noreply.github.com>
2025-04-30 02:13:44 +0900

    042904

--- bpb/ServiceWorker.js
+++ bpb/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Psytofu-BChannel-0.1.1";
 const contentToCache = [
-    "Build/ada6fb5e908c32b1063cb5681fa9f224.loader.js",
-    "Build/cd8e8ff9e40be29073e6d26baecaf299.framework.js",
-    "Build/d2a22bc208a32b9d6a1e178c34339a32.data",
-    "Build/c1ac53b00ea1b1abe44fb33e9b2c7617.wasm",
+    "Build/e79cad82da6df7cb9f2901f5a95068ab.loader.js",
+    "Build/1d4e45eb040dec25a33d20030786c91d.framework.js",
+    "Build/10fcdb97b244285862f6e45ff928a82b.data",
+    "Build/6fc5cc056777658cc402a7fd3fd1d4e0.wasm",
     "TemplateData/style.css"
 
 ];
