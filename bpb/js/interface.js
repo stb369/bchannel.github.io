b5c5e1c66a4238318017aac6530cb9b32681d924
stb369 <35536182+stb369@users.noreply.github.com>
2025-05-01 20:39:43 +0900

    Update interface.js

--- bpb/js/interface.js
+++ bpb/js/interface.js
@@ -19,7 +19,7 @@ hoge = function() {
             var evalString = methodName + '(parameterObject)';
             //eval(evalString);//このevalStringの中に、JavaScriptのソースコードが入っている
             if (typeof window[methodName] === 'function'){
-                window[methodName](param.arg1);
+                window[methodName](param.SolidityCode);
             }else{
                 console.log('関数 ${methodName} は定義されていません');
             }
