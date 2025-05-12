60c66511c140627d2391de66b963087fc5aa1848
stb369 <35536182+stb369@users.noreply.github.com>
2025-05-13 00:54:09 +0900

    Update interface.js

--- bpb/js/interface.js
+++ bpb/js/interface.js
@@ -31,7 +31,7 @@ hoge = function() {
             //const {module} = import(filePath);
             import(filePath).then((myModule) => {
                 if (myModule.default && typeof myModule.default === 'function'){
-                    myModule(parameterObject.arg1,parameterObject.arg2,parameterObject.arg3,parameterObject.arg4,parameterObject.arg5,parameterObject.arg6); // デフォルトエクスポートされた関数を実行
+                    myModule.default(parameterObject.arg1,parameterObject.arg2,parameterObject.arg3,parameterObject.arg4,parameterObject.arg5,parameterObject.arg6); // デフォルトエクスポートされた関数を実行
                 }else{
                     console.log("module error");
                 }
