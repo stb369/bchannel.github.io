efd74f1b213921c1aca2bd370f2e2b2942d305a6
stb369 <35536182+stb369@users.noreply.github.com>
2025-11-09 14:21:04 +0900

    Update f0008.js

--- bpb/js/functions/f0008.js
+++ bpb/js/functions/f0008.js
@@ -22,7 +22,7 @@ export default function f0008(arg1,arg2,arg3,arg4,arg5,arg6) {
     }
 
     async function execSolidity(arg1,arg2,arg3,arg4,arg5,arg6) {
-	  await loadABI("./ABI_11.json");
+	  await loadABI("./js/functions/ABI_11.json");
       const contract = await getContract(arg1);
       const tokenIdA = arg4 / 1000;
       const tokenIdB = arg4 % 1000;
