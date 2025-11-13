068005fbd781ea2cc026383ed03c654d9a905020
stb369 <35536182+stb369@users.noreply.github.com>
2025-11-13 15:54:43 +0900

    Update f0013.js

--- bpb/js/functions/f0013.js
+++ bpb/js/functions/f0013.js
@@ -44,7 +44,7 @@ export default function f0013(arg1,arg2,arg3,arg4,arg5,arg6) {
     		
 			for (const [key, value] of Object.entries(parsed.args)) {
         		// キーが数字ではない（名前付き引数である）ことを確認
-				console.log(i.toString()+"番目:"+parsed.args[i].toString());
+				console.log(key.toString()+":"+value.toString());
 				// BigIntの場合、精度を保つために文字列に変換するか、そのまま残すか選択します。
 				// ここでは扱いやすいように文字列に変換（必要に応じてethers.formatUnitsで変換）
 				if (typeof value === 'bigint') {
