b38294a2da94215b73f45e512f28aa041e218698
stb369 <35536182+stb369@users.noreply.github.com>
2025-11-16 21:39:55 +0900

    251116_02

--- GPUtest/ServiceWorker.js
+++ GPUtest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "InterfaceJSLib-OnEventLog-0.1.1";
 const contentToCache = [
-    "Build/913f1eaa20be3d34a3f53803b13a3dbb.loader.js",
+    "Build/18691a954dd6f30d1d665b8fcba9037a.loader.js",
     "Build/06002a4df9658809582b6900f0b71234.framework.js.unityweb",
-    "Build/48f841556e11f12896de5a4bfc86cc30.data.unityweb",
-    "Build/243fc5866b38b62184562d8502487008.wasm.unityweb",
+    "Build/a68c96c53826e152609c644a93f104a5.data.unityweb",
+    "Build/b6980f4addaa40bb98561d8d88f3ff33.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
