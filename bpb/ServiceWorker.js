1a6cc893e31ec263441a0d6ea4f3c2746cafdca8
stb369 <35536182+stb369@users.noreply.github.com>
2025-11-25 00:56:36 +0900

    251124_22

--- bpb/ServiceWorker.js
+++ bpb/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "InterfaceJSLib-OnEventLog-0.1.1";
 const contentToCache = [
-    "Build/74da7655a343ccaa433e301a873401b6.loader.js",
+    "Build/e8d974b2c11b0496a75403c80ba90682.loader.js",
     "Build/d17f6f80181a6c9ae27871d44588870b.framework.js.unityweb",
-    "Build/90a99b75f2e5563d5f8ad583fbf132e5.data.unityweb",
-    "Build/43ae25cd8dfbb3c7af95502c6765be06.wasm.unityweb",
+    "Build/b26c4139392ad839d98ba739f828e05e.data.unityweb",
+    "Build/1cb2075ad6c901380a64f25fd61eafdc.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
