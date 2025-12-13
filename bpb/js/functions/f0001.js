8a42d5a60f2bbbe8dd451b0999b660ee9d1e7826
stb369 <35536182+stb369@users.noreply.github.com>
2025-12-13 17:34:38 +0900

    Rename parameter 'solidityCode' to 'arg1'

--- bpb/js/functions/f0001.js
+++ bpb/js/functions/f0001.js
@@ -1,5 +1,5 @@
 //このソースファイルをC#のURLで指定する
-export default function f0001(solidityCode,arg2,arg3,arg4,arg5,arg6) {
+export default function f0001(arg1,arg2,arg3,arg4,arg5,arg6) {
             
   let a = arg1+"/"+arg2+"/"+arg3+"/"+arg4.toString()+"/"+arg5.toString()+"/"+arg6.toString();    
   console.log("f0001:",a)
