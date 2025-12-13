650408597f4839c3afdc641f53cdf962c90e3bee
stb369 <35536182+stb369@users.noreply.github.com>
2025-12-13 15:56:20 +0900

    Refactor f0001 to remove Solidity compilation
    Removed Solidity compilation logic and added new functionality to concatenate arguments.

--- bpb/js/functions/f0001.js
+++ bpb/js/functions/f0001.js
@@ -1,45 +1,9 @@
 //このソースファイルをC#のURLで指定する
 export default function f0001(solidityCode,arg2,arg3,arg4,arg5,arg6) {
             
-  const code = (solidityCode);
-  console.log("compile address:"+code);
-  const input = {
-    language: 'Solidity',
-    sources: {
-      'Unique.sol': {
-        content: code
-      }
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['abi', 'evm.bytecode']
-        }
-      }
-    }
-  };
-
-  try {
-    const output = JSON.parse(solc.compile(JSON.stringify(input)));
-    if (output.errors) {
-      const errors = output.errors.map(e => e.formattedMessage).join('\n');
-      console.log('errors:'+errors);
-      SendMessage('ContractCompiler', 'OnCompilationError', errorMessages);
-      return;
-    }
-
-    const contract = output.contracts['Unique.sol'];
-    const name = Object.keys(contract)[0];
-    const abi = contract[name].abi;
-    const bytecode = contract[name].evm.bytecode.object;
-    const result = JSON.stringify({ abi, bytecode });
-
-    console.log('result:' + result);
-    SendMessage('ContractCompiler', 'OnContractCompiled', result);
-  } catch (e) {
-    console.log('e:' + e.message);
-    SendMessage('ContractCompiler', 'OnCompilationError', e.message);
-  }
+  let a = arg1+"/"+arg2+"/"+arg3+"/"+arg4.toString()+"/"+arg5.toString()+"/"+arg6.toString();    
+  console.log("f0001:",a)
+  return a;
 }
           
        
